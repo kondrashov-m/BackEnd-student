d950da8c5910badb6f4c6330e224f5322ec68c7a
kondrashov-m <mig2018kondrashov@gmail.com>
2026-03-02 00:57:02 +0300

    Добавил модель данных Student

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); // добавил Swagger
-
+builder.Services.AddSwaggerGen(); 
 var app = builder.Build();
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = "";
+    public string Group { get; set; } = "";
+    public int Age { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
 
 if (app.Environment.IsDevelopment())
 {
