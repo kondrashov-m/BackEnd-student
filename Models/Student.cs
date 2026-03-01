15e03dd8badeb897e87d5da78987061083481082
kondrashov-m <mig2018kondrashov@gmail.com>
2026-03-02 01:10:41 +0300

    Вынес в отдельную папку модель данных Student

---
+++ Models/Student.cs
@@ -0,0 +1,8 @@
+public class Student
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = "";
+    public string Group { get; set; } = "";
+    public int Age { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); 
+
 var app = builder.Build();
 
-public class Student
-{
-    public int Id { get; set; }
-    public string FullName { get; set; } = "";
-    public string Group { get; set; } = "";
-    public int Age { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
 
 
 if (app.Environment.IsDevelopment())
@@ -19,6 +13,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.MapGet("/", () => "Привет");
 
