9d6d72b69c981b6862444e66f90422f43ca0279b
kondrashov-m <mig2018kondrashov@gmail.com>
2026-04-09 17:34:43 +0300

    feat: implement ProductRepository with in-memory storage

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using DependencyInjectionDemo.Models;
+
+namespace DependencyInjectionDemo.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly List<Product> _products = new();
+    private int _nextId = 1;
+
+    public List<Product> GetAll() => _products;
+
+    public Product? GetById(int id) => _products.FirstOrDefault(p => p.Id == id);
+
+    public Product Add(Product product)
+    {
+        product.Id = _nextId++;
+        product.CreatedAt = DateTime.UtcNow;
+        _products.Add(product);
+        return product;
+    }
+}
