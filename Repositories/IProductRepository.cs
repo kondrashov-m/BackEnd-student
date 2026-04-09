b8d22d6e0a995d2538f3c277ec69a9c58e875798
kondrashov-m <mig2018kondrashov@gmail.com>
2026-04-09 17:34:31 +0300

    feat: add IProductRepository interface

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using DependencyInjectionDemo.Models;
+
+namespace DependencyInjectionDemo.Repositories;
+
+public interface IProductRepository
+{
+    List<Product> GetAll();
+    Product? GetById(int id);
+    Product Add(Product product);
+}
