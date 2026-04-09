a9abd9f3173047428f3ab26910fa9d000591ebed
kondrashov-m <mig2018kondrashov@gmail.com>
2026-04-09 17:35:15 +0300

    feat: add ProductsController with GET and POST endpoints

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using DependencyInjectionDemo.DTOs;
+using DependencyInjectionDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjectionDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_productService.GetAllProducts());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var product = _productService.GetProductById(id);
+        return product == null ? NotFound() : Ok(product);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] ProductCreateDto dto)
+    {
+        if (string.IsNullOrWhiteSpace(dto.Name))
+            return BadRequest("Name is required");
+
+        var created = _productService.CreateProduct(dto);
+        return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+    }
+}
