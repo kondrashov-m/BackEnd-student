fa140a409a234f66498aa8202e9a8d6b988c98f2
kondrashov-m <mig2018kondrashov@gmail.com>
2026-04-09 17:34:24 +0300

    feat: add ProductResponseDto

---
+++ DTOs/ProductResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DependencyInjectionDemo.DTOs;
+
+public class ProductResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
