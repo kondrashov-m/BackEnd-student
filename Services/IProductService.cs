4ffe8399de21d3ced138c910c55b6c0ee893afaf
kondrashov-m <mig2018kondrashov@gmail.com>
2026-04-09 17:34:54 +0300

    feat: add IProductService interface

---
+++ Services/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using DependencyInjectionDemo.DTOs;
+
+namespace DependencyInjectionDemo.Services;
+
+public interface IProductService
+{
+    List<ProductResponseDto> GetAllProducts();
+    ProductResponseDto? GetProductById(int id);
+    ProductResponseDto CreateProduct(ProductCreateDto dto);
+}
