ad0ed8f7de7e7c40e5bf6b24281824ca05dc3cce
kondrashov-m <mig2018kondrashov@gmail.com>
2026-04-09 17:34:09 +0300

    feat: add Product model

---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace DependencyInjectionDemo.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
