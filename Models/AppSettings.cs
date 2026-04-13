2eb8be4f14c8cabaefd9534a4865f50462c2fd58
kondrashov-m <mig2018kondrashov@gmail.com>
2026-04-13 18:47:44 +0300

    feat: updates project files

--- Models/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BackEnd_student.Models;
-
-public class AppSettings
-{
-    public string AppName { get; set; } = string.Empty;
-    public string Version { get; set; } = string.Empty;
-    public int MaxItems { get; set; }
-}
