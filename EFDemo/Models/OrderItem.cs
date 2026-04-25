b1a7047c9c84b5949c30ac13d57f581719f7fc29
kondrashov-m <mig2018kondrashov@gmail.com>
2026-04-25 13:28:28 +0300

    feat: add OrderItem model

---
+++ EFDemo/Models/OrderItem.cs
@@ -0,0 +1,12 @@
+namespace EFDemo.Models;
+
+public class OrderItem
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int OrderId { get; set; }
+    public Order Order { get; set; } = null!;
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+}
