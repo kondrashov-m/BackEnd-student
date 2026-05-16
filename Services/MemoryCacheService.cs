1dd8c03935ad06314840efe05be2e1ce30fd0c04
kondrashov-m <mig2018kondrashov@gmail.com>
2026-05-16 14:12:52 +0300

    feat: add MemoryCacheService implementation

---
+++ Services/MemoryCacheService.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BackEnd_student.Services;
+
+public class MemoryCacheService : ICacheService
+{
+    private readonly IMemoryCache _cache;
+
+    public MemoryCacheService(IMemoryCache cache)
+    {
+        _cache = cache;
+    }
+
+    public T Get<T>(string key)
+    {
+        _cache.TryGetValue(key, out T value);
+        return value;
+    }
+
+    public void Set<T>(string key, T value, TimeSpan? expiration = null)
+    {
+        var options = new MemoryCacheEntryOptions();
+        if (expiration.HasValue)
+            options.AbsoluteExpirationRelativeToNow = expiration;
+        else
+            options.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
+        
+        _cache.Set(key, value, options);
+    }
+
+    public void Remove(string key)
+    {
+        _cache.Remove(key);
+    }
+
+    public bool Exists(string key)
+    {
+        return _cache.TryGetValue(key, out _);
+    }
+}
